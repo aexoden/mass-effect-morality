8352be2d3dbd4164d8e3125824683a5795a8998c
Jason Lynch <jason@aexoden.com>
2025-03-22 14:23:44 -0700

    Detect non-existent option IDs in dependencies.
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- src/data/validation.ts
+++ src/data/validation.ts
@@ -5,6 +5,7 @@ export function validateGameData(data: SectionData[]): { valid: boolean; errors:
     const sectionIds = new Set<string>();
     const groupIds = new Set<string>();
     const choiceIds = new Set<string>();
+    const allChoices = new Map<string, Set<string>>();
 
     // Check for duplicate IDs
     data.forEach((section) => {
@@ -27,20 +28,39 @@ export function validateGameData(data: SectionData[]): { valid: boolean; errors:
                 } else {
                     choiceIds.add(choice.id);
                 }
+
+                allChoices.set(choice.id, new Set<string>());
+
+                // Option IDs don't need to be globally unique, but they do need to be unique within a choice.
+                const optionIds = new Set<string>();
+
+                choice.options.forEach((option) => {
+                    if (optionIds.has(option.id)) {
+                        errors.push(`Duplicate option ID ${option.id} in choice ${choice.id}`);
+                    } else {
+                        optionIds.add(option.id);
+                    }
+
+                    allChoices.get(choice.id)?.add(option.id);
+                });
             });
         });
     });
 
     // Validate dependency relationships
-    const allChoiceIdsSet = new Set([...choiceIds]);
-
     data.forEach((section) => {
         section.groups.forEach((group) => {
             group.choices.forEach((choice) => {
                 // Check that the dependency choiceIds actually exist
                 if (choice.dependsOn) {
                     choice.dependsOn.forEach((dep) => {
-                        if (!allChoiceIdsSet.has(dep.choiceId)) {
+                        if (allChoices.has(dep.choiceId)) {
+                            if (dep.optionId !== "skip" && !allChoices.get(dep.choiceId)?.has(dep.optionId)) {
+                                errors.push(
+                                    `Choice ${choice.id} depends on non-existent option ${dep.optionId} in choice ${dep.choiceId}`,
+                                );
+                            }
+                        } else {
                             errors.push(`Choice ${choice.id} depends on non-existent choice ${dep.choiceId}`);
                         }
                     });
@@ -52,7 +72,16 @@ export function validateGameData(data: SectionData[]): { valid: boolean; errors:
                         option.dependsOn.forEach((dep) => {
                             if (dep.dependsOn) {
                                 dep.dependsOn.forEach((childDep) => {
-                                    if (!allChoiceIdsSet.has(childDep.choiceId)) {
+                                    if (allChoices.has(childDep.choiceId)) {
+                                        if (
+                                            childDep.optionId !== "skip" &&
+                                            !allChoices.get(childDep.choiceId)?.has(childDep.optionId)
+                                        ) {
+                                            errors.push(
+                                                `Option ${option.id} depends on non-existent option ${childDep.optionId} in choice ${childDep.choiceId}`,
+                                            );
+                                        }
+                                    } else {
                                         errors.push(
                                             `Option ${option.id} depends on non-existent choice ${childDep.choiceId}`,
                                         );
